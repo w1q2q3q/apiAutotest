628aa13c8631899a218870f1b18dbf54e17a127d
w1q2q3q <1935417616@qq.com>
2018-11-08 15:17:17 +0800

    Default

--- src/main/java/com/example/demo/Entity/Person.java
+++ src/main/java/com/example/demo/Entity/Person.java
@@ -14,45 +14,4 @@ public class Person {
 
      private int state;
 
-     public int getId() {
-          return id;
-     }
-
-     public void setId(int id) {
-          this.id = id;
-     }
-
-     public String getData() {
-          return data;
-     }
-
-     public void setData(String data) {
-          this.data = data;
-     }
-
-     public String getName() {
-          return name;
-     }
-
-     public void setName(String name) {
-          this.name = name;
-     }
-
-     public int getState() {
-          return state;
-     }
-
-     public void setState(int state) {
-          this.state = state;
-     }
-
-     @Override
-     public String toString() {
-          return "Person{" +
-                  "id=" + id +
-                  ", data='" + data + '\'' +
-                  ", name='" + name + '\'' +
-                  ", state=" + state +
-                  '}';
-     }
 }
