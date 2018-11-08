803718fa426ff28ef641208581982296dd72510e
w1q2q3q <1935417616@qq.com>
2018-11-08 15:17:02 +0800

    Default

---
+++ src/main/java/com/example/demo/Entity/Testcass.java
@@ -0,0 +1,20 @@
+package com.example.demo.Entity;
+
+import lombok.Data;
+
+@Data
+public class Testcass {
+
+    private String Cassname;
+    private String Cassdescribe;
+    private int Cassmodel;
+    private String Url;
+    private int Method;
+    private int Herder;
+    private String HerderList;
+    private int Parameter;
+    private String ParameterList;
+    private int CassAssert;
+    private String AssertList;
+
+}
